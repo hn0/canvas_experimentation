9f4c610117dd406337b979f3a1bfba877e1bb2b5
Hrvoje Novosel <hrvojedotnovosel@gmail.com>
2018-02-03 22:04:55 +0100

    * preparing for the animation

--- js/fib.js
+++ js/fib.js
@@ -1,12 +1,49 @@
 (function(){
 
-    var fib = function()
+
+    var cube = function( mat )
     {
-        this.boxes = 1;
+        this.size = [ 'width', 'height', 'depth' ];
+        this.geom = new THREE.BoxGeometry( .1, .1, .1 );
+        this.mesh = new THREE.Mesh( this.geom, mat );
+        
+        this.vents = {};
+    }
+
+    cube.prototype.update = function()
+    {
+        if( this.geom.parameters.width < 200 ){
+            this.size.forEach( (k) => { this.geom.parameters[k] += 5; } );
+            // this.geom.rotation.x += .2;
+        }
+        else {
+            this.trigger( 'die' );
+        }
     };
+
+    cube.prototype.trigger = function(id)
+    {
+        var next = this.vents[id];
+        while( next ){
+            next.callback( this );
+            next = next.next;
+        }
+    };
+
+    cube.prototype.on = function(id, callback)
+    {
+        this.vents[id] = {
+            callback: callback,
+            next:     this.vents[id] || null
+        };
+    };
+
+
+    var fib = function(){ };
 
     fib.prototype.init = function()
     {
+        this.boxes  = [];
         this.scene  = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);
         this.camera.position.z = 4;
@@ -16,21 +53,31 @@
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( this.renderer.domElement );
 
+        
         this.mat = new THREE.MeshBasicMaterial( { color: '#406030' } );
 
-        this.render();
+        var c = new cube( this.mat );
+        c.on( 'die', (c) => {
+            let i = this.boxes.indexOf( c );
+            if( i > -1 ){
+                this.boxes.splice( i, 1 );
+            }
+        } );
+        this.boxes.push( c );
+        this.scene.add( c.mesh );
+        this.animate();
     };
 
-    fib.prototype.render = function()
+    fib.prototype.animate = function()
     {
-        var box  = new THREE.BoxGeometry( 1, 1, 1 );
-        var cube = new THREE.Mesh( box, this.mat );
 
-        cube.rotation.x -= 0.1;
-        cube.rotation.y += 0.1;
-
-        this.scene.add( cube );
         this.renderer.render( this.scene, this.camera );
+        this.boxes.forEach( (x) => { x.update(); } );
+        console.log( 'here!' )
+
+        if( this.boxes.length ){
+            setTimeout( this.animate.bind( this ), 50);
+        }
     };
 
     window.fib = new fib();
