67466dd06d835dfa4527fee64f2fbc0cb7d1b67c
Hrvoje Novosel <hrvojedotnovosel@gmail.com>
2018-02-04 20:53:35 +0100

    + basic animation & some lighting

--- js/fib.js
+++ js/fib.js
@@ -1,30 +1,51 @@
 (function(){
 
+    //file:///home/user/Documents/fibwebgl/index.html
+    let w = 0;
+    let h = 0;
 
-    var cube = function( mat, pos )
+    var cube = function( mat, i, n )
     {
-        let seed = Math.random() * .3;
+        let scale = Math.min( w / n, 1 )
+        let seed  = Math.random() * .3;
         this.rfnc  = { 
             x: (x)=>{ return x + seed },
             y: (y)=>{ return Math.sin( y - seed); },
             z: (z)=>{ return Math.cos( z + seed); },
         }
-        this.geom  = new THREE.BoxBufferGeometry( 1, 1, 1 );
+        this.geom  = new THREE.BoxBufferGeometry( scale, scale, scale );
         this.mesh  = new THREE.Mesh( this.geom, mat );
         this.vents = {};
         this.mesh.scale.multiply( new THREE.Vector3( .1, .1, .1 ) );
 
-        // how it will look with many of them?
-        this.mesh.position.x = (pos == 1) ? -1 : 1;
+        this.mesh.position.x = (i - n * .5 ) * 2 * scale;
+
+        this.animfnc = this.scalefnc;
     }
 
     cube.prototype.update = function()
     {
+        Object.keys( this.rfnc ).forEach( (k) => {
+            this.mesh.rotation[k] = this.rfnc[k]( this.mesh.rotation[k] );
+        });
+        this.animfnc();
+    };
+
+    cube.prototype.scalefnc = function()
+    {
         if( this.mesh.scale.x < 1 ){
             this.mesh.scale.add( new THREE.Vector3( .05, .05, .05 ) );
-            Object.keys( this.rfnc ).forEach( (k) => {
-                this.mesh.rotation[k] = this.rfnc[k]( this.mesh.rotation[k] );
-            });
+        }
+        else{
+            this.trigger( 'drop' );
+            this.animfnc = this.dropfnc;
+        }
+    };
+
+    cube.prototype.dropfnc = function()
+    {
+        if( this.mesh.position.y > -h ){
+            this.mesh.position.y -= .2;
         }
         else {
             this.trigger( 'die' );
@@ -53,20 +74,29 @@
 
     fib.prototype.init = function()
     {
-        this.seq    = [1, 1];
+        this.seq    = [0, 1];
         this.boxes  = [];
         this.scene  = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);
         this.camera.position.z = 15;
+        
+        h = 2 * Math.tan( .5 * this.camera.fov * Math.PI / 180 ) * 15;
+        w = h * this.camera.aspect;
+        this.camera.position.y = h * -.45;
 
         this.renderer = new THREE.WebGLRenderer({});
         this.renderer.setClearColor( '#fff' );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( this.renderer.domElement );
 
-        
-        this.mat = new THREE.MeshLambertMaterial( { color: '#406030' } );
+        let light = new THREE.AmbientLight( 0x404040, 5 );
+        this.scene.add( light );
 
+        let dirlight = new THREE.DirectionalLight( 0xffffff, 1 );
+        dirlight.position.set( 1, 1, 1 ).normalize();
+        this.scene.add( dirlight );
+
+        this.mat = new THREE.MeshStandardMaterial( { color: '#406030' } );
         this.increment();
         this.animate();
     };
@@ -74,10 +104,15 @@
     fib.prototype.increment = function()
     {
         let n = this.seq[0] + this.seq[1];
-
+        let dropping = 0;
         // now create n elements!
         for( var i=0; i < n; i++ ){
-            let c = new cube( this.mat, (i+1) / n );
+            let c = new cube( this.mat, i, n );
+            c.on( 'drop', (c) => {
+                if( ++dropping == n ){
+                    this.increment();
+                }
+            });
             c.on( 'die', (c) => {
                 let i = this.boxes.indexOf( c );
                 if( i > -1 ){
@@ -89,8 +124,8 @@
         }
 
         this.seq.unshift( n );
-        if( this.seq.length > 5 ){
-            this.seq = [1, 1];
+        if( this.seq.length > 12 ){
+            this.seq = [0, 1];
         }
     };
 
